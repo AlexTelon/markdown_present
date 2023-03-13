9c38113048b54d302aa1a3f875437d56c4e4ce38
Alex Telon <alex.telon@gmail.com>
2023-03-13 12:27:28 +0100

    Improved stuff

---
+++ main.py
@@ -0,0 +1,37 @@
+import markdown
+
+# Read the contents of the markdown file
+with open('presentation.md', 'r') as f:
+    content = f.read()
+
+# Split the content into slides based on headings
+slides = content.split('# ')
+
+# Generate the HTML code for each slide
+slide_html = ''
+for i, slide in enumerate(slides):
+    slide_html += f"""
+      <div class="slide" id="slide{i+1}">
+        {markdown.markdown(slide)}
+      </div>
+    """
+
+# Create the full HTML page with the slide content
+page = f"""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>My Presentation</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    {slide_html}
+  </body>
+</html>
+"""
+
+# Write the HTML page to a file
+with open('template/index.html', 'w') as f:
+    f.write(page)
---
+++ presentation.md
@@ -0,0 +1,39 @@
+# My Presentation
+
+![Sample Image](sample.png)
+
+## Introduction
+
+Welcome to my presentation! Today, I'm going to talk about something very interesting.
+
+## Body
+
+This is the main part of the presentation. Here, I'm going to give you some fascinating information.
+
+- Item 1
+- Item 2
+- Item 3
+
+## Conclusion
+
+And that's all I have for today. I hope you enjoyed the presentation!
+
+---
+
+# Bonus Slides
+
+## Extra Information
+
+Here's some bonus information that I didn't have time to cover in the main part of the presentation.
+
+- Extra item 1
+- Extra item 2
+- Extra item 3
+
+## Acknowledgements
+
+I'd like to thank everyone who helped me put together this presentation.
+
+- Person 1
+- Person 2
+- Person 3
